d08eb9959e36439378344298929bb38466d4d09b
Awatef Hajji <awatefhajji5@gmail.com>
2023-01-23 16:10:44 +0100

    algoPractice

---
+++ algoPractice/week2day1.js
@@ -0,0 +1,34 @@
+
+//var start=""
+// for(var i=0;i<=10;i++){
+//     start+="*";
+//     console.log(start);
+// }
+//odd numbers from 1 to 20
+console.log("odd numbers");
+ for(var i=1;i<=20;i++){
+     if(i%2==0){console.log(i);}
+ }
+//Decreasing Multiples of 3
+console.log("Multiples of 3");
+for(var j=100;j>=1;j--){
+if(j%3==0){console.log(j);}
+ }
+//Print the sequence
+console.log("the sequence result");
+for(var i=4;i>=-3.5;i-=1.5){
+        console.log(i);
+  }
+ //Sigma
+ console.log("sigma");
+ var sum=0;
+for(var i=1;i<=100;i++){
+       sum=sum+i;}
+console.log(sum);
+//Factorial
+console.log("factoriel de 12")
+var fact=1;
+for(var i=2;i<=12;i++){
+       fact=fact*i;}
+console.log(fact);
+     
