5e232cb1f9a97b2930c1c626ed713370855cc98e
Awatef Hajji <awatefhajji5@gmail.com>
2023-01-23 19:38:57 +0100

    buttonClicker

---
+++ buttonClicker/scripte.js
@@ -0,0 +1,10 @@
+function login(id){
+    document.getElementById(id).innerText="Log Out";
+}
+function removeDef(id){
+    e=document.getElementById(id);
+    e.remove()
+}
+function like(element){
+    alert("Ninja was liked");
+}
